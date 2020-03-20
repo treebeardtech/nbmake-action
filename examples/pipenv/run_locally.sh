6e714cbaeb9f924cceaf6872fb49d71e28e8228a
Rabscuttler <laurence.watson@gmail.com>
2020-03-20 13:53:26 +0000

    removed pipenv example as duplicate of reddit_tracker example

--- examples/pipenv/run_locally.sh
+++
@@ -1,5 +0,0 @@
-docker run -p 8000:8000 \
-  --env PORT=8000 \
-  --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/keys/google_creds.json \
-  -v $GOOGLE_APPLICATION_CREDENTIALS:/tmp/keys/google_creds.json:ro
-  8b926f89ded4
