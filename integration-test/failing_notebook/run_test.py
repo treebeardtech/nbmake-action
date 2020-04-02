2ded71bbde5e753fe29e048e44baf944000c6f30
Alex Remedios <alexremedios@alexs-mbp.home>
2020-04-02 16:01:51 +0100

    Split examples into integration tests and e2e tests

--- integration-test/failing_notebook/run_test.py
+++ integration-test/failing_notebook/run_test.py
@@ -1,10 +1,11 @@
 import subprocess
 import sys
+from traceback import format_exc
 
 if __name__ == "__main__":
     try:
         subprocess.check_output(["treebeard", "run", "--watch"])
         sys.exit(1)
     except Exception as ex:
-        print(ex)
+        print(format_exc())
         sys.exit(0)
