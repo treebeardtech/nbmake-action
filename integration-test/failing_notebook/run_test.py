5474e8073ece505d0d480c5b9d7d75bfe293398e
Alex Remedios <alexremedios@alexs-mbp.home>
2020-04-05 14:08:51 +0100

    Add test case

--- integration-test/failing_notebook/run_test.py
+++ integration-test/failing_notebook/run_test.py
@@ -3,8 +3,23 @@ import sys
 from traceback import format_exc
 
 if __name__ == "__main__":
+    # Test local failure fails
     process = subprocess.Popen(
         "treebeard run --watch --local --confirm", shell=True, stdout=subprocess.PIPE
+    )
+    while True:
+        output = process.stdout.readline()
+        if process.poll() is not None:
+            break
+        if output:
+            print(output.strip().decode())  # type: ignore
+    retval = process.poll()
+    print(f"Process exited with status {retval}")
+    assert retval == 1
+
+    # Test remote failure fails
+    process = subprocess.Popen(
+        "treebeard run --watch --confirm", shell=True, stdout=subprocess.PIPE
     )
     while True:
         output = process.stdout.readline()
