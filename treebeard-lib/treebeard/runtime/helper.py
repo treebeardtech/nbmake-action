b9ab136e5bcc3887c76103ba8e3d38446f94878c
Alex Remedios <alexremedios@alexs-mbp.home>
2020-04-06 17:41:23 +0100

    Revert "Content type fix"
    This reverts commit 3348bb1a5dfbee74583d91b0a971984e649a33b7.

--- treebeard-lib/treebeard/runtime/helper.py
+++ treebeard-lib/treebeard/runtime/helper.py
@@ -1,6 +1,5 @@
 import mimetypes
 from datetime import datetime
-from typing import Any, Dict, Optional
 
 import filetype  # type: ignore
 import requests
@@ -13,30 +12,16 @@ def log(message: str):
     print(f'{datetime.now().strftime("%H:%M:%S")}: {message}')
 
 
-def get_content_type(filename: str) -> Optional[str]:
-    guess: Optional[Any] = filetype.guess(filename)  # type: ignore
-    if guess:
-        return guess.mime
-
-    return mimetypes.guess_type(filename)[0]
-
-
-def get_headers(filename: str) -> Dict[str, str]:
-    content_type = get_content_type(filename)
-
-    if content_type:
-        return {"content_type": content_type}
-
-    return {}
-
-
 def upload_artifact(filename: str, upload_path: str):
     log(f"Saving {filename} to {upload_path}")
+    content_type: str = filetype.guess(filename)  # type: ignore
+    if content_type == None:
+        content_type = mimetypes.guess_type(filename)[0] or ""
 
-    headers = get_headers(filename)
     with open(filename, "rb") as data:
         resp: Response = requests.get(
-            f"{file_access_url}/get_upload_url/{upload_path}", params=headers,
+            f"{file_access_url}/get_upload_url/{upload_path}",
+            params={"content_type": content_type},
         )
         signed_url: str = resp.text
-        requests.put(signed_url, data, headers=headers)
+        requests.put(signed_url, data, headers={"Content-Type": content_type})
