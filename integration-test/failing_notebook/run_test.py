7b0c859322c0076ad4ebbd0e256171bb232cc62e
Alex Remedios <alexremedios@alexs-mbp.home>
2020-04-20 12:12:45 +0100

    remove remote test

--- integration-test/failing_notebook/run_test.py
+++ integration-test/failing_notebook/run_test.py
@@ -16,17 +16,3 @@ if __name__ == "__main__":
     retval = process.poll()
     print(f"Process exited with status {retval}")
     assert retval == 1
-
-    print("Test remote failure fails")
-    process = subprocess.Popen(
-        "treebeard run --watch --confirm", shell=True, stdout=subprocess.PIPE
-    )
-    while True:
-        output = process.stdout.readline()
-        if process.poll() is not None:
-            break
-        if output:
-            print(output.strip().decode())  # type: ignore
-    retval = process.poll()
-    print(f"Process exited with status {retval}")
-    assert retval == 1
