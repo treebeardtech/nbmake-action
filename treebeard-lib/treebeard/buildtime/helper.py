5e2875e888ce1fc72673f99eca141c44941ce61b
alex-treebeard <alex@treebeard.io>
2020-06-15 11:54:53 +0100

    tb version

--- treebeard-lib/treebeard/buildtime/helper.py
+++ treebeard-lib/treebeard/buildtime/helper.py
@@ -7,9 +7,10 @@ import docker  # type: ignore
 def run_image(project_id: str, notebook_id: str, run_id: str, image_name: str) -> int:
     client: Any = docker.from_env()  # type: ignore
 
+    pip_treebeard = f"pip install git+https://github.com/treebeardtech/treebeard.git@local-docker#subdirectory=treebeard-lib"
     container = client.containers.run(
         image_name,
-        "bash -c '(which treebeard > /dev/null || pip install treebeard) && treebeard run --dockerless --upload --confirm'",
+        f"bash -c '(which treebeard > /dev/null || {pip_treebeard}) && treebeard run --dockerless --upload --confirm'",
         environment={
             "TREEBEARD_PROJECT_ID": project_id,
             "TREEBEARD_NOTEBOOK_ID": notebook_id,
