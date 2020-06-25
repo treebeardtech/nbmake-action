04b559fbfafa9df82c7efb3e1d31d3cbb5aa91db
alex-treebeard <alex@treebeard.io>
2020-06-25 16:46:36 +0100

    fixes

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -3,6 +3,9 @@ import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 import {treebeardRef} from '../src/conf'
+import yaml from 'yaml'
+import fs from 'fs'
+import dotenv from 'dotenv'
 
 test('throws invalid number', async () => {
   const input = parseInt('foo', 10)
@@ -15,6 +18,13 @@ test('wait 500 ms', async () => {
   const end = new Date()
   var delta = Math.abs(end.getTime() - start.getTime())
   expect(delta).toBeGreaterThan(450)
+})
+
+test('parse notebook-env', async () => {
+  const file = fs.readFileSync('./__tests__/test.yml', 'utf8')
+  const obj = yaml.parse(file)
+  const parsed = dotenv.parse(obj['notebook-env'])
+  expect(parsed).toStrictEqual({FOO: 'bar', baz: '42'})
 })
 
 // test('ref is correct', async () => {
