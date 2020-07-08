1b3191f24105286eae521616e9044763357e3b22
alex-treebeard <alex@treebeard.io>
2020-07-08 17:34:40 +0100

    windows utf fix

--- treebeard-lib/treebeard/__init__.py
+++ treebeard-lib/treebeard/__init__.py
@@ -1,3 +1,15 @@
+import os
+
+if os.name == "nt":
+    try:
+        import sys
+        import io
+
+        sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding="utf-8")
+        sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding="utf-8")
+    except:
+        pass
+
 import warnings
 from typing import Any
 
