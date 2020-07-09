407816f766b61dc8a450b209cd28c94715e98b49
alex-treebeard <alex@treebeard.io>
2020-07-09 15:48:27 +0100

    isort

--- treebeard-lib/treebeard/windows_unicode_fix.py
+++ treebeard-lib/treebeard/windows_unicode_fix.py
@@ -5,7 +5,7 @@ if os.name == "nt":
         import sys
         import io
 
-        sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding="utf-8")
-        sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding="utf-8")
+        sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding="utf-8")  # type: ignore
+        sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding="utf-8")  # type: ignore
     except:
         pass
